d5a6a787448e4307718712ec256400b6e3e876fe
kou-pen <kou-pen@outlook.jp>
2024-04-26 14:47:14 +0900

    update comment

--- common_lib/GearCalcTools.py
+++ common_lib/GearCalcTools.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def calc_gear(const, rad):
+def calc_gear(const: float, rad:float) -> float:
     return const * rad / (2 * np.pi)
 
-def calc_cm_to_rad(const, mm):
+def calc_cm_to_rad(const: float, mm: float) -> float:
     return (2 * np.pi) * mm / const
 
-def calc_deg_to_rad(degrees):
+def calc_deg_to_rad(degrees: float) -> float:
     return degrees * np.pi / 180.0
