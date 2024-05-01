ff59de065ab978579e16f371ff84bf375f59fcac
kou-pen <kou-pen@outlook.jp>
2024-05-01 10:43:54 +0900

    add comment

--- common_lib/ConvertTools.py
+++ common_lib/ConvertTools.py
@@ -1,20 +1,45 @@
 import struct
+"""
+This function converts a float value to a list of integers using the struct module.
 
+:param float_value: The float value to convert
+:type float_value: float
+:return: A list of integers representing the binary representation of the float value
+:rtype: list[int]
+"""
 def float_to_int_list(float_value: float) -> list[int]:
     # float型の値をバイナリに変換
     binary_data = struct.pack('<f', float_value)
-    # バイナリデータをint型に変換し、8ビットずつリストに格納
+    # ��イナリデータをint型に変換し、8ビットずつリストに格��
     int_list = [int(byte) for byte in binary_data]
 
     return int_list
 
+"""
+This function converts a list of integers back to a float value using the struct module.
+
+:param int_list: The list of integers representing the binary representation of the float value
+:type int_list: list[int]
+:return: The float value represented by the list of integers
+:rtype: float
+"""
 def int_list_to_float(int_list: list[int]) -> float:
     # int型のリストをバイナリデータに変換
     binary_data = bytes(int_list)
-    # バイナリデータをfloat型に変換
+    # ��イナリデータをfloat型に変換
     float_value = struct.unpack('<f', binary_data)[0]
     return float_value
 
+"""
+This function converts an integer value to a list of integers using the int.to_bytes() method.
+
+:param int_value: The integer value to convert
+:type int_value: int
+:param dlc: The desired length of the resulting list of integers
+:type dlc: int
+:return: A list of integers representing the binary representation of the integer value
+:rtype: list[int]
+"""
 def int_to_int_list(int_value: int, dlc: int) -> list[int]:
     data = int.to_bytes(int_value, length=dlc, byteorder='little')
     return [int(byte) for byte in data]
--- common_lib/GearCalcTools.py
+++ common_lib/GearCalcTools.py
@@ -1,10 +1,68 @@
+"""
+This module contains functions for calculating gear ratios, converting between centimeters and radians, and converting between degrees and radians.
+
+Functions:
+
+calc_gear(const: float, rad:float) -> float:
+    This function calculates the gear ratio given the module and pitch of the gears.
+    Args:
+        const (float): Constant of the gear ratio.
+        rad (float): Pitch of the gears in radians.
+    Returns:
+        float: Gear ratio.
+
+calc_cm_to_rad(const: float, mm: float) -> float:
+    This function converts centimeters to radians given a constant and a length in millimeters.
+    Args:
+        const (float): Constant of the conversion.
+        mm (float): Length in millimeters.
+    Returns:
+        float: Length in radians.
+
+calc_deg_to_rad(degrees: float) -> float:
+    This function converts degrees to radians.
+    Args:
+        degrees (float): Angle in degrees.
+    Returns:
+        float: Angle in radians.
+"""
+
 import numpy as np
 
 def calc_gear(const: float, rad:float) -> float:
+    """
+    This function calculates the gear ratio given the module and pitch of the gears.
+
+    Args:
+        const (float): Constant of the gear ratio.
+        rad (float): Pitch of the gears in radians.
+
+    Returns:
+        float: Gear ratio.
+    """
     return const * rad / (2 * np.pi)
 
 def calc_cm_to_rad(const: float, mm: float) -> float:
+    """
+    This function converts centimeters to radians given a constant and a length in millimeters.
+
+    Args:
+        const (float): Constant of the conversion.
+        mm (float): Length in millimeters.
+
+    Returns:
+        float: Length in radians.
+    """
     return (2 * np.pi) * mm / const
 
 def calc_deg_to_rad(degrees: float) -> float:
+    """
+    This function converts degrees to radians.
+
+    Args:
+        degrees (float): Angle in degrees.
+
+    Returns:
+        float: Angle in radians.
+    """
     return degrees * np.pi / 180.0
