bf49b44326070141b3e19d028c5f4a1421898e68
kou-pen <kou-pen@outlook.jp>
2024-05-05 15:58:15 +0900

    update

--- common_lib/GearCalcTools.py
+++ common_lib/GearCalcTools.py
@@ -66,3 +66,18 @@ def calc_deg_to_rad(degrees: float) -> float:
         float: Angle in radians.
     """
     return degrees * np.pi / 180.0
+
+import numpy as np
+
+def calc_rad_to_cm(const: float, rad: float) -> float:
+    """
+    This function converts radians to centimeters given a constant and an angle in radians.
+
+    Args:
+        const (float): Constant of the conversion.
+        rad (float): Angle in radians.
+
+    Returns:
+        float: Length in millimeters.
+    """
+    return const * rad / (2 * np.pi)
